c9106223176654613435e884c76db1f7891bdce2
Mark Nelson <marknels@hawaii.edu>
2022-06-24 17:49:58 +0000

    Hello world kernel module

---
+++ rootkit.c
@@ -0,0 +1,22 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Robert Lemon");
+MODULE_DESCRIPTION("My first rootkit");
+MODULE_VERSION("0.00");
+
+static int __init rootkit_init(void)
+{
+	printk(KERN_INFO "rootkit: Loaded >:-)\n");
+
+	return 0;
+}
+
+static void __exit rootkit_exit(void)
+{
+	printk(KERN_INFO "rootkit: Unloaded :-(\n");
+}
+
+module_init(rootkit_init);
+module_exit(rootkit_exit);
